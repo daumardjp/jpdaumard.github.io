71168f22e9cf1513459636cdf5f73b0ab769c61f
daumardjp <154333886+daumardjp@users.noreply.github.com>
2024-01-28 17:58:48 +0100

    Delete _site directory

--- _site/assets/js/lazy-load.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const lazyLoadImages = document.querySelectorAll('img[loading="lazy"]');
-
-    lazyLoadImages.forEach(img => {
-        img.onload = () => {
-            img.classList.add('loaded');
-        };
-    });
-});
